4ae9d3ed3daed29813a682a5412194ccb39e69a4
MJ <mdjacksonvideo@gmail.com>
2018-01-28 17:33:18 -0800

    finish writng AddressDao methods

--- simple-crm/src/main/java/dao/Sql2oAddressDao.java
+++ simple-crm/src/main/java/dao/Sql2oAddressDao.java
@@ -1,28 +1,60 @@
 package dao;
 
 import models.Address;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 
 import java.util.List;
 
 public class Sql2oAddressDao implements AddressDao {
 
+    private final Sql2o sql2o;
+
+    public Sql2oAddressDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
     @Override
     public void add(Address address) {
-
+        String sql = "INSERT INTO addresses (street, postalcode, country, city, state, clientId) VALUES (:street, :postalcode, :country, :city, :state, :clientId)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(address)
+                    .executeUpdate()
+                    .getKey();
+            address.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
     public List<Address> getAllAddressesByClient(int clientId) {
-        return null;
+        try (Connection con = sql2o.open()) {
+            return con.createQuery("SELECT * FROM addresses WHERE clientId = :clientId")
+                    .addParameter("clientId", clientId)
+                    .executeAndFetch(Address.class);
+        }
     }
 
     @Override
     public List<Address> getAll() {
-        return null;
-    }
+            try (Connection con = sql2o.open()) {
+                return con.createQuery("SELECT * FROM addresses")
+                        .executeAndFetch(Address.class);
+            }
+        }
 
     @Override
     public void deleteById(int id) {
-
+        String sql = "DELETE from addresses WHERE id=:id";
+        try (Connection con = sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex);
+        }
     }
 }
--- simple-crm/src/main/java/dao/Sql2oContactDao.java
+++ simple-crm/src/main/java/dao/Sql2oContactDao.java
@@ -1,14 +1,32 @@
 package dao;
 
 import models.Contact;
+import org.sql2o.Connection;
+import org.sql2o.Sql2oException;
+import org.sql2o.Sql2o;
 
 import java.util.List;
 
 public class Sql2oContactDao implements ContactDao {
+    private final Sql2o sql2o;
+
+    public Sql2oContactDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
 
     @Override
     public void add(Contact contact) {
-
+        String sql = "INSERT INTO contacts (firstname, lastname, position, contactPhone, contactEmail, clientId) VALUES (:firstname, :lastname, :position, :contactPhone, :contactEmail, :clientId)";
+        try (Connection con = sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .bind(address)
+                    .executeUpdate()
+                    .getKey();
+            address.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
     }
 
     @Override
